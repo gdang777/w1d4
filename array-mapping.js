2112de87749e4ece20cc6171e5428a54a2b8400c
Gaurav <netgary7@gmail.com>
2018-09-27 20:56:54 +0000

    Aray mapping exercise added

---
+++ array-mapping.js
@@ -0,0 +1,13 @@
+var input = [
+  { x: 3, y: 4 },
+  { x: 12, y: 5 },
+  { x: 8, y: 15 }
+];
+// console.log(input;
+var result = input.map(function(value){
+   return Math.sqrt(Math.pow(value.x, 2) + Math.pow(value.y, 2))
+});
+console.log(result);
+console.log(result[0] === 5)
+console.log(result[1] === 13);
+console.log(result[2] === 17);
